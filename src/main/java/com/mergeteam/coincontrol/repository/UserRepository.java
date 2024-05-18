d83122ba6a4500b50d3d243782d15624e61600e0
Artem Yuran <yuoran371@gmail.com>
2024-05-18 03:08:52 +0300

    Add implemented User, start implementing UserService and UserRepository

---
+++ src/main/java/com/mergeteam/coincontrol/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.mergeteam.coincontrol.repository;
+
+import com.mergeteam.coincontrol.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+//    findAllBy
+
+}
