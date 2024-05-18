5648c936aa726c82b3cb108cce9f34fafae644c8
Artem Yuran <yuoran371@gmail.com>
2024-05-18 17:58:16 +0300

    Fix merge conflict

--- src/main/java/com/mergeteam/coincontrol/api/UserRestController.java
+++ src/main/java/com/mergeteam/coincontrol/api/UserRestController.java
@@ -1,7 +1,10 @@
 package com.mergeteam.coincontrol.api;
 
+import com.mergeteam.coincontrol.dto.CreateUserDto;
+import com.mergeteam.coincontrol.dto.ReadUserDto;
 import com.mergeteam.coincontrol.dto.UpdateUserDto;
 import com.mergeteam.coincontrol.dto.WalletDto;
+import com.mergeteam.coincontrol.entity.ExpenseTransaction;
 import com.mergeteam.coincontrol.entity.User;
 import com.mergeteam.coincontrol.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -23,8 +26,18 @@ public class UserRestController {
     }
 
     @PutMapping("/{id}")
-    public User updateUser(@PathVariable UUID id, UpdateUserDto userDto) {
+    public ReadUserDto updateUser(@PathVariable UUID id, UpdateUserDto userDto) {
         return userService.updateUser(id, userDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable UUID id) {
+        userService.deleteById(id);
+    }
+
+    @PostMapping("/{id}")
+    public ReadUserDto saveUser(CreateUserDto createUserDto) {
+        return userService.save(createUserDto);
     }
 
 
